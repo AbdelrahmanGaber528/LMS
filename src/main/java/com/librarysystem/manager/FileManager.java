3871bcb969cc25c18f77b9ebc47b2060bc17cc79
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-11-28 08:58:15 +0200

    restructure the code

---
+++ src/main/java/com/librarysystem/manager/FileManager.java
@@ -0,0 +1,11 @@
+package com.librarysystem.manager;
+
+import java.util.List;
+
+public class FileManager {
+    public static void writeInFile(){};
+//    public static List<String> readFromFile(){};
+    public static void appendToFile(){};
+    public static void clearFile(){};
+    public static void deleteFile(){};
+}
