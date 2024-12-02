6a3e412a61c68fa956dcce909e43089f5e53dc7a
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-12-02 03:33:07 +0200

    delete the functions from fileManagerInterace

--- src/main/java/com/librarysystem/util/LMSFileManager.java
+++ src/main/java/com/librarysystem/util/LMSFileManager.java
@@ -1,53 +1,5 @@
 package com.librarysystem.util;
 
-import com.librarysystem.models.User;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
 public class LMSFileManager implements LMSFileMangerOperations {
-
-    @Override
-    public boolean readFromFile(String path) {
-        try(BufferedReader reader = new BufferedReader(new FileReader(path))) {
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } ;
-        return false;
-    }
-    @Override
-    public boolean writeToFile(String path, List<String> lines) {
-        return false;
-    }
-
-    @Override
-    public boolean appendToFile(String filePath, List<String> lines) {
-        return false;
-    }
-
-    @Override
-    public User searchInFile(String filePath, String keyword) {
-        return null;
-    }
-
-    @Override
-    public void updateLineInFile(String filePath, String oldLine, String newLine) {
-
-    }
-
-    @Override
-    public boolean removeLineFromFile(String filePath, String lineToRemove) {
-        return false;
-    }
-
-    @Override
-    public boolean clearFile(String filePath) {
-        return false;
-    }
-
+    
 }
--- src/main/java/com/librarysystem/util/LMSFileMangerOperations.java
+++ src/main/java/com/librarysystem/util/LMSFileMangerOperations.java
@@ -1,17 +1,8 @@
 package com.librarysystem.util;
 
-import com.librarysystem.models.User;
 
-import java.util.List;
-import java.util.Map;
 
 public interface LMSFileMangerOperations {
-    public boolean writeToFile(String filePath, List<String> lines);
-    public boolean readFromFile(String filePath);
-    public boolean appendToFile(String filePath, List<String> lines);
-    public User searchInFile(String filePath, String keyword);
-    public void updateLineInFile(String filePath, String oldLine, String newLine);
-    public boolean removeLineFromFile(String filePath, String lineToRemove);
-    public boolean clearFile(String filePath);
+
 }
 //private Map<Integer, Book> booksMap = new HashMap<>();
