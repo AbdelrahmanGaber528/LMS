ec1701909d363fe32e04d622ec6bfca32439e9e7
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-12-14 20:39:19 +0200

    add book Columns

--- src/main/java/com/librarysystem/service/LoginService.java
+++ src/main/java/com/librarysystem/service/LoginService.java
@@ -1,5 +1,16 @@
 package com.librarysystem.service;
 
+import com.librarysystem.dao.AccountDAO;
+import com.librarysystem.util.LMSFileManager;
+
 public class LoginService {
 
+    private final LMSFileManager fileManager;
+    private final AccountDAO accountDAO ;
+
+    public LoginService(){
+        fileManager = new LMSFileManager("src\\main\\docs\\Accounts.txt");
+        accountDAO = new AccountDAO();
+    }
+    
 }
--- src/main/java/com/librarysystem/util/ColumnName.java
+++ src/main/java/com/librarysystem/util/ColumnName.java
@@ -1,7 +1,8 @@
 package com.librarysystem.util;
 
 public enum ColumnName {
-
+    // Book
+    BOOK_ID,
     // TRANSACTION
     TRANSACTION_ID,//0
     PATRON_ACCOUNT_ID,//1
@@ -10,8 +11,16 @@ public enum ColumnName {
     BOOK_TITLE ,
     TOTAL_AMOUNT,
 
+    // Book continue
+    BOOK_AUTHOR,
+    BOOK_PRICE,
+    CATEGORY,
+    STATUS,
+    AMOUNT,
+
     // Admin
     ADMIN_Account_ID,
+
     // ACCOUNT
     USER_ID,
     USER_NAME ,
