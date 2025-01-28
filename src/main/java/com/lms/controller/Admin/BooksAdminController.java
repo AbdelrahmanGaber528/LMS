b49e89580d5c5364807c9e0e19757e3dc623125e
AbdelrahmanGaber <abdelrahmangaber002@gmail.com>
2025-01-28 22:29:22 +0200

    tst2

--- src/main/java/com/lms/controller/Admin/BooksAdminController.java
+++ src/main/java/com/lms/controller/Admin/BooksAdminController.java
@@ -36,8 +36,6 @@ public class BooksAdminController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
-
         ObservableList<String> sort_list = FXCollections.observableArrayList(
                 "Name", "ID", "Date", "Category", "Author"
         );
